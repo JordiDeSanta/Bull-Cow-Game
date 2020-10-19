2959f22ddff91d2fb3070dbd0b2fb8ca754f8300
Jordi <jordi.bravo.bad@gmail.com>
2020-10-19 18:07:23 -0300

    Hello World Test

--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -11,11 +11,14 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
 	GENERATED_BODY()
 
-	public:
+public:
+
 	virtual void BeginPlay() override;
+
 	virtual void OnInput(const FString& Input) override;
 
-	// Your declarations go below!
-	private:
+	
+	
+private:
 	
 };
